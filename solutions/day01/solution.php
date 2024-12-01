798a904d5dd96348fabe43e4476cedac002f22de
thatoldplatitude <itsfirstnamedotlastname@tutanota.com>
2024-12-01 13:48:29 -0500

    Better descriptors in comments.

--- solutions/day01/solution.php
+++ solutions/day01/solution.php
@@ -1,12 +1,12 @@
 <?php
 /** 
  * Pair up the numbers and measure how far apart they are.
- * Pair up the smallest number in the left list with the smallest number in the right list,
+ * Pair up the smallest number in the left array with the smallest number in the right array,
  * then the second-smallest left number with the second-smallest right number, and so on.
  * 
  * Data set contains 2columns of numbers (unordered).
- * Read & parse data to 2 lists.
- * Order lists.
+ * Read & parse data to 2 arrays.
+ * Order arrays.
  * Sum the differences of each idx.
  */
   $dataInput = __DIR__ . '/../../inputs/data.txt';
